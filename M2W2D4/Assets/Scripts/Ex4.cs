2c442c2210bb485066c96f70cda562a1749ee4d1
ThinkDifferent003 <rolandbibal3@gmail.com>
2025-10-24 19:06:37 +0200

    Update Ex4.cs

--- M2W2D4/Assets/Scripts/Ex4.cs
+++ M2W2D4/Assets/Scripts/Ex4.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Ex4 : MonoBehaviour
 {
+    [SerializeField] int startNumber = 5;
+    [SerializeField] int amount = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < amount; i++) ;
+        Debug.Log(startNumber + amount);
     }
 
     // Update is called once per frame
