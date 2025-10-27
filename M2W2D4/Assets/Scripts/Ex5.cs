987be942f5332bb5da3fc05947b1897ad25b0b9c
ThinkDifferent003 <rolandbibal3@gmail.com>
2025-10-27 17:58:23 +0100

    ex5

--- M2W2D4/Assets/Scripts/Ex5.cs
+++ M2W2D4/Assets/Scripts/Ex5.cs
@@ -6,17 +6,27 @@ public class Ex5 : MonoBehaviour
 {
     [SerializeField] private int numberToFind;
     [SerializeField] private int arraySize;
+    [SerializeField] private int numberOffset;
     // Start is called before the first frame update
     void Start()
     {
-        int[] numeri = new int[5];
+        int[] numeri = new int[arraySize];
+
+
         for (int i = 0; i < numeri.Length; i++)
-            {
+        {
+            numeri[i] = Random.Range(numberToFind - numberOffset,numberToFind + numberOffset);
+
             if (numeri[i] == numberToFind)
-                Debug.Log("found");
+            {
+                Debug.Log("Found");
+            }
             else
-                Debug.Log("Not Found");
+            { Debug.Log("Not Found"); }
+           
         }
+        Debug.Log("Iterations Finished");
+
     }
 
     // Update is called once per frame
